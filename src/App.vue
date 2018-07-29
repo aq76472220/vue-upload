c5446918254ee89d8c7d4b522e26a890b56e858b
ludawei <793198567@qq.com>
2018-07-30 00:21:56 +0800

    代码提交

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+     
     <router-view/>
   </div>
 </template>
 
 <script>
+import vueUpload from './pages/components/upload/upload'
+
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  // components: {vueUpload}
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ 
 </style>
